2cc52662f2f57648d9f905f0289b69283cbea07a
PhucTB <92681505+PhucTB@users.noreply.github.com>
2021-10-18 21:46:26 +0700

    up2

--- website_mvc/config/config.php
+++ website_mvc/config/config.php
@@ -1,6 +1,7 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "web_banhang");
+
+define("DB_HOST", "mysql5027.site4now.net");
+define("DB_USER", "a7b40b_webdien");
+define("DB_PASS", "phucvipboy38");
+define("DB_NAME", "db_a7b40b_webdien");
 ?>
